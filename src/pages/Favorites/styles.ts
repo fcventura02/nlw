e7173914e980949aea06989c9ab31480ef590409
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-08-10 01:14:22 -0300

    TeacherFavorites created

---
+++ src/pages/Favorites/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0,
+        backgroundColor: '#f0f0f7',
+        justifyContent: "center",
+    },
+    teacherList:{
+        marginTop: -40,
+    },
+
+});
+
+export default styles;
