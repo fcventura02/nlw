718b3d6bb4eecb19fc8d33fb0c308019f0cd4b68
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-08-10 01:12:11 -0300

    header created with directions

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -5,17 +5,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
-import Study from '../pages/Study';
+import StudyTabs from './StudyTabs';
 
 const { Navigator, Screen } = createStackNavigator();
 
 function AppStack() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{ headerShown:false }}>
+            <Navigator screenOptions={{ headerShown: false }}>
                 <Screen name='Landing' component={Landing} />
                 <Screen name='GiveClasses' component={GiveClasses} />
-                <Screen name='Study' component={Study} />
+                <Screen name='StudyTabs' component={StudyTabs} />
             </Navigator>
         </NavigationContainer>
     );
