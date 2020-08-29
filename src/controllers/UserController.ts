aa8f76833c267c8273d2ab94870678fe2ada72f6
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-08-28 22:34:14 -0300

    Created the Users class

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,100 @@
+import { Response, Request } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import db from '../database/connection';
+
+
+export default class UserController {
+
+    async authUser(req: Request, res: Response) {
+        const { email, password } = req.body;
+        console.log({ email, password })
+        try {
+            const user = await db('users').select('*').where('email', '=', email).then((user) => {
+                return user
+            });
+
+            //se tem email válido
+            if (!user)
+                return res.status(401).json({ err: 'Falha na autenticação' });
+
+            //Validando senha
+            const authCompare = await bcrypt.compare(password, user[0].password_hash)
+            if (!authCompare)
+                return res.status(401).json({ err: 'Falha na autenticação' });
+            //se tudo Ok retonar usuário
+            const token: string = jwt.sign({
+                user_id: user[0].id
+            },
+                process.env.JWT_TOKEN || 'DarthVader',
+                {
+                    expiresIn: "1d",
+                });
+
+            return res.status(200).json({
+                token,
+                id: user[0].id,
+                name: user[0].name,
+                avatar: user[0].avatar,
+                whatsapp: user[0].whatsapp,
+                email: user[0].email,
+                bio: user[0].bio
+            });
+        } catch (err) {
+            //caso tenha falha na busca do BD
+            return res.status(401).json({ err: 'Falha na autenticação' });
+        }
+    };
+
+    async createUser(req: Request, res: Response) {
+        const {
+            name,
+            avatar,
+            whatsapp,
+            email,
+            password,
+            bio,
+        } = req.body;
+
+        await bcrypt.hash(password, 10, async (errBcrypt, password_hash) => {
+            if (errBcrypt)
+                return res.status(500).json({ err: errBcrypt })
+            await db('users').insert({
+                name,
+                avatar,
+                whatsapp,
+                email,
+                password_hash,
+                bio,
+            });
+            return res.status(201).send('ok')
+        })
+    };
+
+    async updateUser(req: Request, res: Response) {
+        const { user_id } = req.currentUser
+
+        const {
+            name,
+            avatar,
+            whatsapp,
+            email,
+            bio,
+        } = req.body;
+
+        try {
+            await db('users').where('id', '=', user_id).update({
+                name,
+                avatar,
+                whatsapp,
+                email,
+                bio,
+            });
+            return res.status(201).send('ok')
+        } catch (error) {
+            return res.status(401).send('ok')
+        }
+
+    };
+
+}
